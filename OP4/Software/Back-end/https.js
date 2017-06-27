82f006ae8429372d6ba5983da47f7082cf661c2b
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-27 14:52:50 +0200

    Changed the keys

--- OP4/Software/Back-end/https.js
+++ OP4/Software/Back-end/https.js
@@ -2,8 +2,8 @@ var restify = require('restify');
 fs = require('fs');
 
 var https_options = {
-  key: fs.readFileSync('/var/www/project78/.well-known/acme-challenge/yourdomain.key'),
-  certificate: fs.readFileSync('/var/www/project78/.well-known/acme-challenge/yourdomain.csr')
+  key: fs.readFileSync('key.pem', 'utf8'),
+  certificate: fs.readFileSync('server.crt', 'utf8')
 };
 var https_server = restify.createServer(https_options);
 
