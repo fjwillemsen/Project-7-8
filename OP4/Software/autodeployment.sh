1895e8566bd1304655819145f40b22e6675c016f
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-28 10:03:40 +0200

    Revamped autodeployment

--- OP4/Software/autodeployment.sh
+++ OP4/Software/autodeployment.sh
@@ -1,5 +1,6 @@
-cd OP4/Software/Back-end
-pm2 stop all
-pm2 delete all
-pm2 start main.js -i max --watch
-pm2 show main
+cd OP4/Software
+npm install --production
+npm update
+cd Back-end/       
+lsof -i :8081 | grep "nodejs" | cut -d " " -f3 | xargs kill -9
+nodejs main.js &
