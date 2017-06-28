5bf134b439df836e83fb71469ee5deccccf37f17
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-28 13:22:37 +0200

    Modified headers Added testposts

---
+++ OP4/Software/Back-end/testpost.js
@@ -0,0 +1,18 @@
+$.ajax({
+    url: "https://xtremeresq.ddns.net:8082/testpost",
+    async: false,
+    type: "POST",
+    data: {
+        blob: {wob:"1",job:"2", ar:[1,2,{a:'b'}]}
+    },
+
+    contentType: "application/x-www-form-urlencoded", //This is what made the difference.
+    dataType: "json",
+    success: function(data) {
+        console.log(data);
+    },
+    error: function() { 
+        console.log("error");
+    }
+});
+console.log("done");
