24bf4f8cfd2812ed0136a357895dcad46e545a07
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-29 16:04:01 +0200

    Added unit test Autodeployment now runs and reports tests Added Unit Testing framework Mocha

--- OP4/Software/autodeployment.sh
+++ OP4/Software/autodeployment.sh
@@ -4,4 +4,5 @@ npm update
 kill -9 $(lsof -t -i:8081)     
 BUILD_ID=dontKillMe nodejs Back-end/main.js &
 netstat -ntlp | grep LISTEN
+MOCHA_FILE=./jenkins-test-results.xml ./node_modules/.bin/mocha tests/** --reporter mocha-junit-reporter
 exit 0
