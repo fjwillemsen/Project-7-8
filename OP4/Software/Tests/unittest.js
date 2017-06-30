edac4ad9c57f2fefe9c1e8388e8ffe65fa3e9adb
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-30 10:45:29 +0200

    Added new unit tests

--- OP4/Software/Tests/unittest.js
+++ OP4/Software/Tests/unittest.js
@@ -1,8 +1,37 @@
+var moment = require('moment');
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+
+
+describe('Date Time getter', function() {
+    describe('getDateTime()', function() {
+        it('should return current date and time in YYYYMMDDHHmmSS format', function() {
+        assert.equal(14, getDateTime().length);
+        });
     });
-  });
 });
+
+describe('Empty Checker', function() {
+    describe('isEmpty()', function() {
+        it('should return true if empty', function() {
+            var object = {};
+            assert.equal(true, isEmpty(object));
+        });
+
+        it('should return false if not empty', function() {
+            var object = { hello: "world" };
+            assert.equal(true, isEmpty(object));
+        });
+    });
+});
+
+// Returns YYYYMMDDHHmmSS formated DateTime
+function getDateTime() {
+    return moment(new Date()).format('YYYYMMDDHHmmSS');
+}
+
+// Returns true if empty, false if not
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+
